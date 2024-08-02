3028e4befd6ed8100ae90adf5cddf78160e145d9
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-02 16:22:08 +0700

    deploy

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use('/api/users', userRoutes);
 app.get("/",(req,res)=>{
     res.send("Hello Api vercel i want to  add  more.......")
   })
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
