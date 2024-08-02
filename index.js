69e0497612e4c555e962968e9ddee731581cbf52
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-02 16:27:00 +0700

    deploy

--- index.js
+++ index.js
@@ -8,12 +8,35 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-app.get("/",(req,res)=>{
-    res.send("Hello Api vercel i want to  add  more.......")
-  })
+app.get("/", (req, res) => {
+    res.send("Hello Api vercel i want to add more.......");
+});
+
+// Remove the following lines as Vercel handles the port internally
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //     console.log(`Server running on port ${PORT}`);
 // });
 
 module.exports = app;
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const app = express();
+
+// app.use(bodyParser.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/users', userRoutes);
+// app.get("/",(req,res)=>{
+//     res.send("Hello Api vercel i want to  add  more.......")
+//   })
+// // const PORT = process.env.PORT || 5000;
+// // app.listen(PORT, () => {
+// //     console.log(`Server running on port ${PORT}`);
+// // });
+
+// module.exports = app;
