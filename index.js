ff345685ec96203641aade6550e83cb05ec88b27
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-04 00:09:30 +0700

    try to deploy again

--- index.js
+++ index.js
@@ -107,9 +107,9 @@ app.delete('/delete/:id', (req, res) => {
 });
 
 // Use environment variable for port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
