98df41845197ce21415d131b81780d7c601b5303
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-10 09:30:32 +0700

    deploy

--- config/db.js
+++ config/db.js
@@ -1,20 +1,36 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
-    user: 'uh2hxkyn6dvplh0q',
-    password: '89ZhwLv0lOFI50utVfUU',
-    database: 'bly8lzj6xutf9s2k61rc',
-    port: 3306
-},
-  
-);
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySQL Connected...');
+const db = mysql.createPool({
+  connectionLimit: 10, // Adjust based on your needs
+  host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
+  user: 'uh2hxkyn6dvplh0q',
+  password: '89ZhwLv0lOFI50utVfUU',
+  database: 'bly8lzj6xutf9s2k61rc',
+  port: 3306
 });
-// jwtSecret: 'JLAJO12@#)@*(#jsljdalsj121923#*@@*#3uj293';
+
+// Function to get a connection from the pool
+function getConnection(callback) {
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting a connection from the pool:', err);
+      return callback(err);
+    }
+    callback(null, connection);
+  });
+}
+
+// Example query usage
+getConnection((err, connection) => {
+  if (err) throw err;
+
+  connection.query('SELECT 1 + 1 AS solution', (error, results) => {
+    connection.release(); // Release the connection back to the pool
+
+    if (error) throw error;
+    console.log('The solution is:', results[0].solution);
+  });
+});
+
+// Export the pool
 module.exports = db;
