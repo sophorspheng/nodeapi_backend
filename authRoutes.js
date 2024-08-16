3b7e24bcd62a392e7a3761efefa6edc3a51bb138
sophorspheng <122344909+sophorspheng@users.noreply.github.com>
2024-08-16 22:12:43 +0700

    Add files via upload

---
+++ authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authenticateJWT = require('../middleware/authenticateJWT')
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const storage = multer.memoryStorage(); // Store file in memory
+const upload = multer({ storage: storage });
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+router.get('/data', authController.getData);
+router.delete('/delete/:id',authenticateJWT, authController.deleteImage);
+router.post('/upload',authenticateJWT,upload.array('images', 10),authController.uploadData)
+
+module.exports = router;
