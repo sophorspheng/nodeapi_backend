240ec195322290806d072687cda4ccbff327a539
sophorspheng <122344909+sophorspheng@users.noreply.github.com>
2024-08-16 22:14:15 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
-router.get('/data',authenticateJWT, authController.getData);
+router.get('/data', authController.getData);
 router.delete('/delete/:id',authenticateJWT, authController.deleteImage);
 router.post('/upload',authenticateJWT,upload.array('images', 10),authController.uploadData)
 
