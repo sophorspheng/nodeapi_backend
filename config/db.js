33de641da33214fbe85f0dc944bac973657c88e1
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-20 10:56:42 +0700

    deploy db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 // const mysql = require('mysql2/promise');
 const db = mysql.createPool({
-  connectionLimit: 10, // Adjust based on your needs
- host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
-user: 'uh2hxkyn6dvplh0q',
-password: '89ZhwLv0lOFI50utVfUU',
-database: 'bly8lzj6xutf9s2k61rc',
-port: 3306
+  // connectionLimit: 10, // Adjust based on your needs
+ host: '897.h.filess.io',
+user: 'MobileECM_truthfully',
+password: '4f68677060f5d016ffc2162b8be0ab3bce885cb1',
+database: 'MobileECM_truthfully',
+port: 3307
 });
 
 // host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
@@ -17,27 +17,27 @@ port: 3306
 
 
 
-function getConnection(callback) {
-  db.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting a connection from the pool:', err);
-      return callback(err);
-    }
-    callback(null, connection);
-  });
-}
+// function getConnection(callback) {
+//   db.getConnection((err, connection) => {
+//     if (err) {
+//       console.error('Error getting a connection from the pool:', err);
+//       return callback(err);
+//     }
+//     callback(null, connection);
+//   });
+// }
 
-// Example query usage
-getConnection((err, connection) => {
-  if (err) throw err;
+// // Example query usage
+// getConnection((err, connection) => {
+//   if (err) throw err;
 
-  connection.query('SELECT 1 + 1 AS solution', (error, results) => {
-    connection.release(); // Release the connection back to the pool
+//   connection.query('SELECT 1 + 1 AS solution', (error, results) => {
+//     connection.release(); // Release the connection back to the pool
 
-    if (error) throw error;
-    console.log('The solution is:', results[0].solution);
-  });
-});
+//     if (error) throw error;
+//     console.log('The solution is:', results[0].solution);
+//   });
+// });
 
 
 // Export the pool
