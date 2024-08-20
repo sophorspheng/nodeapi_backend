62ad0a01dfe6c3aa2f8ed1ebb1bc7c2943527f33
Sophors Pheng <sophorspheng.num@gmail.com>
2024-08-20 11:38:49 +0700

    deploy

--- config/db.js
+++ config/db.js
@@ -2,18 +2,20 @@ const mysql = require('mysql');
 // const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   connectionLimit: 10,
- host: '897.h.filess.io',
-user: 'MobileECM_truthfully',
-password: '4f68677060f5d016ffc2162b8be0ab3bce885cb1',
-database: 'MobileECM_truthfully',
-port: 3307
+  host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
+  user: 'uh2hxkyn6dvplh0q',
+  password: '89ZhwLv0lOFI50utVfUU',
+  database: 'bly8lzj6xutf9s2k61rc',
+  port: 3306
 });
 
-// host: 'bly8lzj6xutf9s2k61rc-mysql.services.clever-cloud.com',
-// user: 'uh2hxkyn6dvplh0q',
-// password: '89ZhwLv0lOFI50utVfUU',
-// database: 'bly8lzj6xutf9s2k61rc',
-// port: 3306
+// host: '897.h.filess.io',
+// user: 'MobileECM_truthfully',
+// password: '4f68677060f5d016ffc2162b8be0ab3bce885cb1',
+// database: 'MobileECM_truthfully',
+// port: 3307
+
+/
 
 
 
